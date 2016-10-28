cefc4645a3d6393f015082ec2fc4f28d9b762bc6
bbidoree <evajean@gmail.com>
2016-10-28 22:18:29 +0900

    Update boardcover.cpp

--- boardcover.cpp
+++ boardcover.cpp
@@ -7,6 +7,17 @@ const int coverType[4][3][2] = {
 };
 
 bool set(vector<vector<int>>& board, int y, int x, int type, int delta) {
+    bool ok=true;
+    
+    for(int i=0; i<3; i++) {
+        const int ny = y+coverType[type][i][0];
+        const int nx = x+coverType[type][i][1];
+        if(ny<0 || ny>=board.size() || nx<0 || nx>=board.size())
+            ok=false;
+        else if(board[ny][nx] += delta) > 1)
+            ok=false;
+    }
+    return ok;
 }
 
 int cover(vector<vector<int>>&board) {
