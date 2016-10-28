26155462f440f1f8013c1aad978d61f99c837aba
bbidoree <evajean@gmail.com>
2016-10-28 20:04:42 +0900

    Update picnic.cpp

--- picnic.cpp
+++ picnic.cpp
@@ -12,7 +12,14 @@ int countPairings(bool taken[10]) {
     
     //Base Case#1
     if (firstFree==-1) return 1;
-    
     int ret=0;
+    
+    for(int pairWith=firstFree+1; pairWith<n; ++pairWith) {
+        if(!taken[firstFree] && areFriends[firstFree][pairWith]) {
+            taken[firstFree] = taken[pairWith] = true;
+            ret += countPairings(taken);
+            taken[firstFree] = taken[pairWith] = false;
+        }
+    }
     
 }
