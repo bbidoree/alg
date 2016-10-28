e0b974b651b71aa1ca3c5596e8aa6f699768c546
bbidoree <evajean@gmail.com>
2016-10-28 19:43:11 +0900

    Update picnic.cpp

--- picnic.cpp
+++ picnic.cpp
@@ -1,2 +1,18 @@
 int n;
 bool areFriends[10][10];
+
+int countPairings(bool taken[10]) {
+    int firstFree = -1;
+    for(int i=0; i<n;++i) {
+        if(!taken[i]) {
+            firstFree=i;
+            break;
+        }
+    }
+    
+    //Base Case#1
+    if (firstFree==-1) return 1;
+    
+    int ret=0;
+    
+}
